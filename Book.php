708f6aad264c4388dec80c6011036d0587be030c
Gaur12345 <40723415+Gaur12345@users.noreply.github.com>
2020-06-23 09:24:20 +0530

    Add files via upload

---
+++ Book.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+use Laravel\Passport\HasApiTokens;
+
+
+
+class Book extends Model
+{
+    //
+	protected $table="books";
+	public $timestamps=false;
+}
